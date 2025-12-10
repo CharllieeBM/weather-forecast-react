f47aa1d9f2a6f076c825f369d634ceb3f863c948
CharllieeBM <cburylb2@gmail.com>
2025-12-10 22:28:50 +0200

    added components from previous project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
-      <p>Welcome to my React application.</p>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.delac.io/" target="_blank" rel="noreferrer">
+            Matt Delac
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
